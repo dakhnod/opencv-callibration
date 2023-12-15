fad82012b1ad8ed595976f0afa8c15a25f229197
Daniel Dakhno <dakhnod@gmail.com>
2023-12-15 10:35:23 +0100

    fixed callibration to "work" with actual image

--- callibration.py
+++ callibration.py
@@ -4,8 +4,8 @@ import glob
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((6 * 4,3), np.float32)
+objp[:,:2] = np.mgrid[0:6,0:4].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -14,14 +14,30 @@ for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv.findChessboardCorners(gray, (6, 4), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
+
+        # create a copy to avoid the having the lines drawn onto it on the next line
+        original = img.copy()
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.drawChessboardCorners(img, (6, 4), corners2, ret)
         cv.imshow('img', img)
-        cv.waitKey(500)
+        cv.waitKey()
+
+        ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+        h,  w = original.shape[:2]
+        newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+        dst = cv.undistort(original, mtx, dist, None, newcameramtx)
+
+        x, y, w, h = roi
+        dst = dst[y:y+h, x:x+w]
+        cv.imshow('distorted', dst)
+        cv.waitKey()
+        # cv.imwrite('calibresult.png', dst)
+    else:
+        print('could not find corners')
 cv.destroyAllWindows()
